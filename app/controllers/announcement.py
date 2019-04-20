bd0e0972f7bee9d908729ad83428f93666c839a2
Kenny Nelson <nelsonk@berea.edu>
2019-04-20 17:53:50 +0000

    Finished the feature that allows students to make announcements on the application.

--- app/controllers/announcement.py
+++ app/controllers/announcement.py
@@ -3,8 +3,8 @@ from app.allImports import *
      
 @app.route("/announcements", methods=["GET"])
 def getAnnouncements():
-    
-    return render_template('announcements.html')
+    announcements = Announcement.select()
+    return render_template('announcements.html', announcements = announcements)
 
  
 @app.route("/makeAnnouncement", methods=["GET"])
@@ -13,20 +13,54 @@ def makeAnnouncements():
     return render_template('makeAnnouncements.html')
 
 
-@app.route("/submitAnnouncement", methods=["GET"])
+@app.route("/submitAnnouncement", methods=["POST"])
 def submitAnnouncements():
     
-    return render_template('makeAnnouncements.html')
+    author = User.get(User.username == "nelsonk")
+    
+    data = request.form 
+    
+    now = datetime.datetime.now()
+    
+    date = now.strftime("%Y-%m-%d")
+    
+    time = now.strftime("%H:%M")
+    
+    content = data['announcement']
+    
+    announcement = Announcement(author = author.UID, date = date, time = time, content = content)
+    
+    announcement.save()
+    
+    return redirect(url_for('getAnnouncements'))
     
     
-@app.route("/respondAnnouncement", methods=["GET"])
-def respondAnnouncements():
-    
-    return render_template('respondAnnouncement.html')
+@app.route("/respondAnnouncement/<author>", methods=["GET"])
+def respondAnnouncements(author):
+    return render_template('respondAnnouncement.html', author = author)
     
 
     
-@app.route("/respondAnnouncementSubmit", methods=["GET"])
-def submitAnnouncementResponse():
+@app.route("/respondAnnouncementSubmit/<author>", methods=["POST"])
+def submitAnnouncementResponse(author):
+    
+    responder = User.get(User.username == "nelsonk")
+    
+    author = User.get(User.UID == int(author))
+    
+    data = request.form 
+    
+    
+    now = datetime.datetime.now()
+    
+    date = now.strftime("%Y-%m-%d")
+    
+    time = now.strftime("%H:%M")
+    
+    content = "[Response to Announcement] " + data['announcement']
+
+    message = Message(sender = responder.UID, receiver = author.UID, content = content, date = date, time = time)
+    
+    message.save()
     
     return redirect(url_for("getAnnouncements"))
